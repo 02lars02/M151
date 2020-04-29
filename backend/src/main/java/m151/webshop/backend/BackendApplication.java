63f33e1148107d0bf3edfb4d2034c86749526697
Lars Ghezzi <lars_ghezzi@sluz.ch>
2020-04-29 10:04:23 +0200

    initial commit

---
+++ backend/src/main/java/m151/webshop/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package m151.webshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
