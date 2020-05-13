7e8816614a7cd05dde2156885d7c038e44b55fc2
Lars Ghezzi <lars_ghezzi@sluz.ch>
2020-05-13 08:34:24 +0200

    initialize spring boot new with cache dependencie

--- backend/src/main/java/m151/webshop/backend/BackendApplication.java
+++ backend/src/main/java/m151/webshop/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package m151.webshop.backend;
+package M151.webshop.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
