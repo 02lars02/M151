45c8cf7daf72ad7b1da99a84069d6866150d8078
Lars Ghezzi <lars.ghezzi@bison-group.com>
2020-05-20 09:20:09 +0200

    add response status on product

--- backend/src/main/java/m151/webshop/backend/controller/ProductController.java
+++ backend/src/main/java/m151/webshop/backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package m151.webshop.backend.controller;
 import m151.webshop.backend.model.Product;
 import m151.webshop.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,10 +25,12 @@ public class ProductController {
     }
 
     @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.OK)
     public void add(@RequestBody Product fruit) {
         productService.add(fruit);
     }
 
     @GetMapping("/delete/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
     public void delete(@PathVariable int id) { productService.delete(id); }
 }
