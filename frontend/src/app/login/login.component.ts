56365b7b61322f5147cb1221abdd868037ef0805
Lars Ghezzi <lars.ghezzi@bison-group.com>
2020-06-03 13:56:54 +0200

    finalize login on frontend

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../auth/AuthenticationService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { AuthenticationService } from '../../auth/AuthenticationService';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthenticationService) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
       .value;
     const password = (document.getElementById('password') as HTMLInputElement)
       .value;
-    this.loginReguest(username, password).then(res => console.log(res));
+    this.loginReguest(username, password).then(res => this.router.navigate(['']));
   }
 
   async loginReguest(username, password) {
